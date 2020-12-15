fcaac50bda0d016c35782aaf2618560aa8bf42b8
Federico Macías <fma.abogado@gmail.com>
2020-12-14 22:09:17 -0600

    Se termina con la funcionalidad de cargar y de eliminar en el componente de notas

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { format } from "timeago.js";
 
 export default class NotesList extends Component {
   state = {
@@ -7,11 +8,25 @@ export default class NotesList extends Component {
   };
 
   async componentDidMount() {
+    //const res = await axios.get("http://localhost:5000/api/notes");
+    //notes es en ralidad res.data.notes
+    //checar siempre la respuesta que nos da axios por la consola
+    //this.setState({ notes: res.data.notes });
+    this.getNotes();
+  }
+
+  async getNotes() {
     const res = await axios.get("http://localhost:5000/api/notes");
+    //notes es en ralidad res.data.notes
+    //checar siempre la respuesta que nos da axios por la consola
     this.setState({ notes: res.data.notes });
   }
 
   //
+  deleteNote = async (id) => {
+    await axios.delete("http://localhost:5000/api/notes/" + id);
+    this.getNotes();
+  };
 
   render() {
     return (
@@ -19,8 +34,21 @@ export default class NotesList extends Component {
         {this.state.notes.map((note) => (
           <div className="col-md-4 p-2" key={note._id}>
             <div className="card">
+              <div className="card-header">
+                <h4>{note.title}</h4>
+              </div>
               <div className="card-body">
                 <p>{note.content}</p>
+                <p>{note.author}</p>
+                <p>{format(note.date)}</p>
+              </div>
+              <div className="card-footer">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => this.deleteNote(note._id)}
+                >
+                  DELETE
+                </button>
               </div>
             </div>
           </div>
