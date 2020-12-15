2245cdbde1b2f5eb1caeaf98c35f508bb7a1e9f9
Federico Macías <fma.abogado@gmail.com>
2020-12-14 21:41:24 -0600

    Por fin se consigue renderizar con la función map los datos de las notas

--- frontend/src/components/NotesList.jsx
+++ frontend/src/components/NotesList.jsx
@@ -8,8 +8,7 @@ export default class NotesList extends Component {
 
   async componentDidMount() {
     const res = await axios.get("http://localhost:5000/api/notes");
-    this.setState({ notes: res.data });
-    console.log(this.state.notes);
+    this.setState({ notes: res.data.notes });
   }
 
   //
@@ -17,9 +16,15 @@ export default class NotesList extends Component {
   render() {
     return (
       <div className="row">
-        {
-          //
-        }
+        {this.state.notes.map((note) => (
+          <div className="col-md-4 p-2" key={note._id}>
+            <div className="card">
+              <div className="card-body">
+                <p>{note.content}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
